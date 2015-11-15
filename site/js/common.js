e99c15ac797cbdc015b289603beabe8be684b790
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2015-11-15 15:44:08 +0200

    fixes upd

--- site/js/common.js
+++ site/js/common.js
@@ -1,19 +1,46 @@
 $(document).ready(function() {
-	
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+	//plus minus for input
+	function number() {
+		var number = $(".js-number");
+		number.each(function() {
+			var input = $(this).find("input");
+			var max_number = input.attr("max");
+			var plus = $(this).find(".js-number-plus");
+			var minus = $(this).find(".js-number-minus");
+			plus.on("click", function() {
+				var val = +(input.val());
+				if (val >= max_number) {
+					return false
+				} else {
+					val += 1;
+					input.val(val);
+				}
+			});
+			minus.on("click", function() {
+				var val = +(input.val());
+				if (val > 1) {
+					val -= 1;
+					input.val(val);
+				}
+				return false;
+			});
+			input.on("change", function() {
+				var val = +$(this).val();
+				if (val > max_number) {
+					val = max_number;
+					$(this).val(val);
+				}
+				if (val == '') {
+					val = 1;
+					$(this).val(val);
+				}
+			});
+		});
+	}
+	number();
+	$('.js-tooltip').popup();
+	$('.ui.checkbox').checkbox();
+	$('.ui.radio.checkbox').checkbox();
 
-	console.log($('body').html());
 });
